d0df224c07c3c137b31beab9797f4f175ee5fd83
ZQD <52098184+jojolilian@users.noreply.github.com>
2020-02-25 01:05:56 +0100

    Update find_missing_label.py

--- tool_scripts/find_missing_label.py
+++ tool_scripts/find_missing_label.py
@@ -5,21 +5,21 @@ Created on Sun Jan 26 18:40:10 2020
 @author: zqd-d
 """
 import os
-filePath = 'C:\\Users\\zqd-d\\Desktop\\train\\'
+filePath = 'C:\\Users\\zqd-d\\Desktop\\train\\' #path of the folder which contains all training data in png format.
 
-fpng_names =os.listdir(filePath)
+fpng_names =os.listdir(filePath) #names of the labels 
 #print(fpng_names)
-print(len(fpng_names))
+print(len(fpng_names))  #get the account of the labels
 i=0
-with open('C:\\Users\\zqd-d\\Desktop\\groundtruth_train\\all_to_split.tsv','r')as flabel:
+with open('C:\\Users\\zqd-d\\Desktop\\groundtruth_train\\all_to_split.tsv','r')as flabel: #open the checking file which contains labels(groundtruth)
     lines = flabel.readlines()
     
     for line in lines:
         
-        if line.split()[0]+'.png' in fpng_names:
+        if line.split()[0]+'.png' in fpng_names: #if the label is overthere, account plus 1
             i+=1
             
-        else:
+        else: #if it isn't there print its name
             print(line)
             #print(line.split()[0]+'.png')
-    print(i)
+    print(i) #the account of contained groundtruth
