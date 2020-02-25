cc63e7d32ea1a262f270dfd9f76800d205f6270b
ZQD <52098184+jojolilian@users.noreply.github.com>
2020-02-25 01:14:59 +0100

    Update find_unknown_tokens.py

--- tool_scripts/find_unknown_tokens.py
+++ tool_scripts/find_unknown_tokens.py
@@ -8,9 +8,9 @@ Created on Wed Jan 29 23:22:57 2020
 from dataset_find import CrohmeDataset
 from torchvision import transforms
 
-gt_train = "C:\\Users\\zqd-d\\Desktop\\test2016_groundtruth.tsv"
+gt_train = "C:\\Users\\zqd-d\\Desktop\\test2016_groundtruth.tsv"  #path of the file containing groundtruth to be checked
 
-tokensfile = "D:\\##SelfLernen\\PythonPj\\接收\\model_1_res1000\\data\\tokens_new.tsv"
+tokensfile = "D:\\##SelfLernen\\PythonPj\\接收\\model_1_res1000\\data\\tokens_new.tsv" # path of tokens.tsv (token dictionary)
 root = "C:\\Users\\zqd-d\\Desktop\\train\\"
 
 
@@ -32,6 +32,6 @@ train_dataset = CrohmeDataset(
         gt_train, tokensfile,  root=root,transform=transformers
     )#crop=options.crop,
 
-
+#if the code could run, it means all training data is correctly packaged.
 
 
