293fd684d91084ae199f966256a8cfe7a4fdf8a0
ZQD <52098184+jojolilian@users.noreply.github.com>
2020-02-25 01:44:05 +0100

    Update tokensNum.py

--- tool_scripts/tokensNum.py
+++ tool_scripts/tokensNum.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jan 25 23:13:58 2020
-
+Find the number of different symbol types without counting <start><end><pad>
 @author: zqd-d
 """
 
-with open('./tokens_dl.tsv','r')as f:
+with open('./tokens.tsv','r')as f: #path of tokens.tsv
     tokens = f.readline().split()
     
     print(len(tokens))
